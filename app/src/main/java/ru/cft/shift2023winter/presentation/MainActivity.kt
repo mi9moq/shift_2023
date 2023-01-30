7594c241b6a611988fc6e100dd61dc7fd3dd01d6
Denis <denis5918@gmai.com>
2023-01-31 00:06:21 +0700

    Добавил базовую реализацию списка аниме

--- app/src/main/java/ru/cft/shift2023winter/presentation/MainActivity.kt
+++ app/src/main/java/ru/cft/shift2023winter/presentation/MainActivity.kt
@@ -1,13 +1,31 @@
 package ru.cft.shift2023winter.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.cft.shift2023winter.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.ViewModelProvider
+import ru.cft.shift2023winter.presentation.main.BestAnimeScreen
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+	val component by lazy {
+		(application as AnimeApplication).component
+	}
+
+	@Inject
+	lateinit var viewModelFactory: ViewModelFactory
+
+	private val viewModel by lazy {
+		ViewModelProvider(this,viewModelFactory)[AnimeListViewModel::class.java]
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
+		component.inject(this)
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		viewModel.loadData()
+		setContent {
+			BestAnimeScreen(viewModel = viewModel)
+		}
 	}
 }
