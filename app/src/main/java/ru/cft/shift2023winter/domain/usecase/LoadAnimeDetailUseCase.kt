acf02f3324aa70d9dc2d5b59bf9e6805a945a7cd
Denis <denis5918@gmai.com>
2023-01-30 11:13:08 +0700

    Рефактор юзкейсов и репозитория

--- app/src/main/java/ru/cft/shift2023winter/domain/usecase/LoadAnimeDetailUseCase.kt
+++ app/src/main/java/ru/cft/shift2023winter/domain/usecase/LoadAnimeDetailUseCase.kt
@@ -1,10 +1,12 @@
 package ru.cft.shift2023winter.domain.usecase
 
+import ru.cft.shift2023winter.domain.entity.AnimeDetailInfo
 import ru.cft.shift2023winter.domain.repository.AnimeRepository
 import javax.inject.Inject
 
 class LoadAnimeDetailUseCase @Inject constructor(
     private val repository: AnimeRepository
 ) {
-    operator fun invoke(animeId: String) = repository.loadAnimeDetail(animeId = animeId)
+    suspend operator fun invoke(animeId: String): AnimeDetailInfo =
+        repository.loadAnimeDetail(animeId = animeId)
 }
--- app/src/main/java/ru/cft/shift2023winter/domain/usecase/LoadBestAnimeListUseCase.kt
+++ app/src/main/java/ru/cft/shift2023winter/domain/usecase/LoadBestAnimeListUseCase.kt
@@ -1,10 +1,11 @@
 package ru.cft.shift2023winter.domain.usecase
 
+import ru.cft.shift2023winter.domain.entity.AnimeItem
 import ru.cft.shift2023winter.domain.repository.AnimeRepository
 import javax.inject.Inject
 
 class LoadBestAnimeListUseCase @Inject constructor(
     private val repository: AnimeRepository
 ) {
-    operator fun invoke() = repository.loadBestAnimeList()
+    suspend operator fun invoke(): List<AnimeItem> = repository.loadBestAnimeList()
 }
