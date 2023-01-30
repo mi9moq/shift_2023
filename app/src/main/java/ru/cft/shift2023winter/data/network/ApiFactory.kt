fb5247f99a505ca474d65682d70ac05eb88cfc0c
Denis <denis5918@gmai.com>
2023-01-31 00:00:01 +0700

    Изменил структуру под компоуз

--- app/src/main/java/ru/cft/shift2023winter/data/network/ApiFactory.kt
+++ app/src/main/java/ru/cft/shift2023winter/data/network/ApiFactory.kt
@@ -1,13 +1,22 @@
 package ru.cft.shift2023winter.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
     private const val BASE_URL = "https://api.consumet.org/anime/gogoanime/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
