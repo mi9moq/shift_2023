622dfe71845810945f0cfe49eecfc9f0f0c109fa
Denis <denis5918@gmai.com>
2023-01-30 11:22:19 +0700

    Переименовал мапперы и изменил реализацию репозитория

--- app/src/main/java/ru/cft/shift2023winter/data/repository/AnimeRepositoryImpl.kt
+++ app/src/main/java/ru/cft/shift2023winter/data/repository/AnimeRepositoryImpl.kt
@@ -1,27 +1,21 @@
 package ru.cft.shift2023winter.data.repository
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import ru.cft.shift2023winter.data.mapper.MapperAnimeDetail
-import ru.cft.shift2023winter.data.mapper.MapperAnimeList
-import ru.cft.shift2023winter.data.network.ApiService
+import ru.cft.shift2023winter.data.mapper.AnimeDetailMapper
+import ru.cft.shift2023winter.data.mapper.AnimeListMapper
+import ru.cft.shift2023winter.data.network.AnimeApi
 import ru.cft.shift2023winter.domain.entity.AnimeDetailInfo
 import ru.cft.shift2023winter.domain.entity.AnimeItem
 import ru.cft.shift2023winter.domain.repository.AnimeRepository
 import javax.inject.Inject
 
 class AnimeRepositoryImpl @Inject constructor(
-    private val mapperAnimeList: MapperAnimeList,
-    private val mapperAnimeDetail: MapperAnimeDetail,
-    private val apiService: ApiService
+    private val animeListMapper: AnimeListMapper,
+    private val animeDetailMapper: AnimeDetailMapper,
+    private val animeApi: AnimeApi
 ) : AnimeRepository {
-    override fun loadBestAnimeList(): Flow<List<AnimeItem>> = flow {
-        val list = apiService.loadPopularAnimeList().animeList
-        emit(mapperAnimeList.mapPopularAnimeListToListEntity(list))
-    }
+    override suspend fun loadBestAnimeList(): List<AnimeItem> =
+        animeListMapper.mapPopularAnimeListToListEntity(animeApi.loadPopularAnimeList().animeList)
 
-    override fun loadAnimeDetail(animeId: String): Flow<AnimeDetailInfo> = flow {
-        val animeDetail = apiService.loadAnimeDetails(animeId)
-        emit(mapperAnimeDetail.mapAnimeDetailInfoDtoToEntity(animeDetail))
-    }
+    override suspend fun loadAnimeDetail(animeId: String): AnimeDetailInfo =
+        animeDetailMapper.mapAnimeDetailInfoDtoToEntity(animeApi.loadAnimeDetails(animeId))
 }
