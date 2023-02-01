bd1d266f793267851788001e9bbd31697311e09d
Denis <denis5918@gmai.com>
2023-02-01 18:41:59 +0700

    Лобавил стейт и вью модель для детальной информации

---
+++ app/src/main/java/ru/cft/shift2023winter/presentation/animedetail/AnimeDetailUiState.kt
@@ -0,0 +1,14 @@
+package ru.cft.shift2023winter.presentation.animedetail
+
+import ru.cft.shift2023winter.domain.entity.AnimeDetailInfo
+
+sealed interface AnimeDetailUiState{
+
+    object Initial: AnimeDetailUiState
+
+    object Loading: AnimeDetailUiState
+
+    data class Content(val animeInfo: AnimeDetailInfo): AnimeDetailUiState
+
+    data class Error(val message: String?): AnimeDetailUiState
+}
---
+++ app/src/main/java/ru/cft/shift2023winter/presentation/animedetail/AnimeDetailViewModel.kt
@@ -0,0 +1,33 @@
+package ru.cft.shift2023winter.presentation.animedetail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.cft.shift2023winter.domain.usecase.LoadAnimeDetailUseCase
+
+class AnimeDetailViewModel(
+    private val loadAnimeDetailUseCase: LoadAnimeDetailUseCase
+) : ViewModel() {
+
+    private val _state: MutableStateFlow<AnimeDetailUiState> =
+        MutableStateFlow(AnimeDetailUiState.Initial)
+    val state: StateFlow<AnimeDetailUiState> = _state.asStateFlow()
+
+    fun loadAnimeDetail(animeId: String) {
+        viewModelScope.launch {
+            _state.value = AnimeDetailUiState.Loading
+            try {
+                val animeDetail = loadAnimeDetailUseCase(animeId)
+                _state.value = AnimeDetailUiState.Content(animeDetail)
+            } catch (rethrow: CancellationException) {
+                throw rethrow
+            } catch (e: Exception) {
+                _state.value = AnimeDetailUiState.Error(e.message)
+            }
+        }
+    }
+}
